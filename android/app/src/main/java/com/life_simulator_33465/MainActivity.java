aee29828f894c7528fdfe866352be41c5dce8f7a
Crowdbotics <team@crowdbotics.com>
2022-02-09 09:16:18 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/life_simulator_33465/MainActivity.java
@@ -0,0 +1,15 @@
+package com.life_simulator_33465;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "life_simulator_33465";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "life_simulator_33465",
+  url: "https://life_simulator_33465.botics.co",
+  api: "https://life_simulator_33465.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
